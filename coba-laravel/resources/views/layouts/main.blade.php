8b3f962c929222e159ce9a6d058f355a6b13dfa5
yghpras <yoghip2@gmail.com>
2022-10-25 16:00:28 +0700

    Mempelajari Model, Collection & Controller

--- coba-laravel/resources/views/layouts/main.blade.php
+++ coba-laravel/resources/views/layouts/main.blade.php
@@ -6,9 +6,10 @@
     <title>WPU Blog | {{ $title }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
+
   <body>
     
-    @include('partials.navbar');
+    @include('partials.navbar')
 
     <div class="Container m-4">
         @yield('Container')
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,55 +32,6 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-        "title" => "Judul Post Pertama",
-        "slug" => "Judul-post-pertama",
-        "author" => "Yoghi Prasetiyo",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim fugiat alias a soluta. Nobis harum quisquam officiis ratione dolor et in illum voluptatibus error maiores porro exercitationem, aliquid vero magnam similique accusantium delectus sequi voluptates qui ut earum labore perferendis obcaecati praesentium. Quod veniam, dolor repellat dolores dicta excepturi exercitationem quis nisi. Vel obcaecati iusto, labore ratione placeat expedita impedit eos? Explicabo obcaecati possimus numquam nulla porro unde quod, aliquam, libero corrupti sapiente itaque delectus excepturi rerum quis earum reiciendis?"
-        ],
-        [
-        "title" => "Judul Post Kedua",
-        "slug" => "Judul-post-kedua",
-        "author" => "OkyBoy",
-        "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum accusamus iusto quos et sed, nesciunt magnam error. Cum nostrum, vero quasi voluptatum deleniti quas dolorem hic saepe dolor tempore tempora ullam libero adipisci porro corrupti ducimus? Rerum, inventore blanditiis. Aut possimus, praesentium atque ad doloremque harum officiis dolorem doloribus sed et, alias omnis, enim fugit laborum totam ipsam placeat molestias numquam suscipit! Rem nostrum, non unde, autem a obcaecati laudantium quae dolores mollitia, blanditiis repudiandae ad dolorem! Rerum natus est fugit, consequatur rem possimus commodi. Amet facilis quos quidem iure quo distinctio repellendus similique officiis magni laboriosam, quas incidunt eligendi."
-        ],
-    ];
+Route::get('/blog', [PostController::class, 'index']);
 
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-// //Halaman single post
-
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-        "title" => "Judul Post Pertama",
-        "slug" => "Judul-post-pertama",
-        "author" => "Yoghi Prasetiyo",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim fugiat alias a soluta. Nobis harum quisquam officiis ratione dolor et in illum voluptatibus error maiores porro exercitationem, aliquid vero magnam similique accusantium delectus sequi voluptates qui ut earum labore perferendis obcaecati praesentium. Quod veniam, dolor repellat dolores dicta excepturi exercitationem quis nisi. Vel obcaecati iusto, labore ratione placeat expedita impedit eos? Explicabo obcaecati possimus numquam nulla porro unde quod, aliquam, libero corrupti sapiente itaque delectus excepturi rerum quis earum reiciendis?"
-        ],
-        [
-        "title" => "Judul Post Kedua",
-        "slug" => "Judul-post-kedua",
-        "author" => "OkyBoy",
-        "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum accusamus iusto quos et sed, nesciunt magnam error. Cum nostrum, vero quasi voluptatum deleniti quas dolorem hic saepe dolor tempore tempora ullam libero adipisci porro corrupti ducimus? Rerum, inventore blanditiis. Aut possimus, praesentium atque ad doloremque harum officiis dolorem doloribus sed et, alias omnis, enim fugit laborum totam ipsam placeat molestias numquam suscipit! Rem nostrum, non unde, autem a obcaecati laudantium quae dolores mollitia, blanditiis repudiandae ad dolorem! Rerum natus est fugit, consequatur rem possimus commodi. Amet facilis quos quidem iure quo distinctio repellendus similique officiis magni laboriosam, quas incidunt eligendi."
-        ],
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
