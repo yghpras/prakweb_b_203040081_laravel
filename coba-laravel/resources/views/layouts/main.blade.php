51b36f2ff88cfdadd2714733922b1e72301bb244
yghpras <yoghip2@gmail.com>
2022-11-04 22:34:49 +0700

    Mempelajari View Login, Registration & User Registration

--- coba-laravel/resources/views/layouts/main.blade.php
+++ coba-laravel/resources/views/layouts/main.blade.php
@@ -3,8 +3,17 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>WPU Blog | {{ $title }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    {{-- Bootstrap --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    
+    
+    {{-- My Style --}}
+    <link rel="stylesheet" href="/css/style.css">
+
+    <title>WPU Blog | {{ $title }}</title>
+    
   </head>
 
   <body>
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Models\Post;
-use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Route;
-use App\Models\User;
+
 use App\Models\Category;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,5 +50,11 @@ Route::get('/categories', function() {
     ]);
 });
 
+
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
 
 
