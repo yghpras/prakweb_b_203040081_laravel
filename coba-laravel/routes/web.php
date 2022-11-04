00ac5a028c85a0f0a4f91189db7a930ff38605ce
yghpras <yoghip2@gmail.com>
2022-11-05 01:44:47 +0700

    Mempelajari Dashboard UI

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Models\Category;
-use Illuminate\Support\Facades\Route;
 
+
+use App\Models\Category;
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -61,4 +63,9 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function(){
+    return view('dashboard.index');
+} )->middleware('auth');
+
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
